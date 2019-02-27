83367a997a53371382171af303cf3f6b6a7e6767
osvaldoarreola <37843538+osvaldoarreola@users.noreply.github.com>
2019-02-27 14:11:08 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,58 @@
+var audio = document.getElementById("you-win");
+a = document.getElementById("slot1");
+b = document.getElementById("slot2");
+c = document.getElementById("slot3");
 
 // cycle images in the following order: CHERRY --> LEMON --> ORANGE
 function nextImage(el){
-if (el.src.match("images/cherry_slot.PNG")){
-// if the image is a cherry, change it to a lemon!
-el.src = "images/lemon_slot.PNG";
-} else if (el.src.match("images/lemon_slot.PNG")){
-// if the image is a lemon, change it to an orange!
-el.src = "images/orange_slot.PNG";
-}else if (el.src.match("images/orange_slot.PNG")){
-// if the image is an orange, change it to a cherry!
-el.src = "images/waterberry.png"
-}else if (el.src.match("images/waterberry.PNG")){
-// if the image is an orange, change it to a cherry!
-el.src = "images/seven.png"
-}else if (el.src.match("images/seven.PNG")){
-// if the image is an orange, change it to a cherry!
-el.src = "images/barman.png"
-}else if (el.src.match("images/barman.PNG)){
-// if the image is an orange, change it to a cherry!
-el.src = "images/cherry_slot.PNG"
-}
- else {
-// do nothing
-}
+	if (el.src.match("images/cherry_slot.PNG")){
+		el.src = "images/lemon_slot.PNG";
+		// if the image is a cherry, change it to a lemon!
+	} else if (el.src.match("images/lemon_slot.PNG")){
+		el.src = "images/orange_slot.PNG";
+		// if the image is a lemon, change it to an orange!
+	} else if (el.src.match("images/orange_slot.PNG")){
+		el.src = "images/cherry_slot.PNG";
+		// if the image is an orange, change it to a cherry!
+	} else {
+		// do nothing
+	}
 }
 
+function nextImage1(el){
+	if (el.src.match("images/lemon_slot.PNG")){
+		el.src = "images/barman.PNG";
+	} else if (el.src.match("images/barman.PNG")){
+		el.src = "images/cherry_slot.PNG";
+	} else if (el.src.match("images/cherry_slot.PNG")){
+		el.src = "images/lemon_slot.PNG";
+	} else {
+		
+	}
+}
 
-// LEMON --> BAR --> CHERRY
-// function nextImage(el){}
+function nextImages(el){
+	if (el.src.match("images/orange_slot.PNG")){
+		el.src = "images/cherry_slot.PNG";
+	} else if (el.src.match("images/cherry_slot.PNG")){
+		el.src = "images/seven.PNG";
+	} else if (el.src.match("images/seven_slot.PNG")){
+		el.src = "images/lemon_slot.png";
+	} else if (el.src.match("images/lemon_slot.png")){
+		el.src = "images/orange_slot.PNG";
+	} else {
+		
+	}
+}
 
-
-// ORANGE --> CHERRY --> SEVEN --> WATERMELON
-//function nextImage(el){}
-
-function playSound() {
+function  checkConfiguration(el){
+	if(a.src.match("images/cherry_slot.PNG") && b.src.match("images/cherry_slot.PNG") && c.src.match("images/cherry_slot.PNG")){
+		a.classList.add("opaque");
+		b.classList.add("opaque");
+		c.classList.add("opaque");
+		audio.play();
+	} else {
+		
+	}
 	// when the JACKPOT! button is clicked, play the slot machine sound
-	document.getElementById('you-win').play();
 }
